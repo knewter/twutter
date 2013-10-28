08bdfe9959b218515e1707a8cb50529a4e4108f0
Josh Adams <josh@isotope11.com>
2013-10-28 11:17:27 -0500

    Add ability to configure a client, sort of
    Still very much a WIP

--- lib/twutter/rest/client.ex
+++ lib/twutter/rest/client.ex
@@ -10,6 +10,18 @@ defmodule Twutter.REST.Client do
     :gen_server.call client, :consumer_key
   end
 
+  def consumer_secret(client) do
+    :gen_server.call client, :consumer_secret
+  end
+
+  def access_token(client) do
+    :gen_server.call client, :access_token
+  end
+
+  def access_token_secret(client) do
+    :gen_server.call client, :access_token_secret
+  end
+
   ### GenServer API
   def init(auth) do
     {:ok, auth}
@@ -17,5 +29,14 @@ defmodule Twutter.REST.Client do
 
   def handle_call(:consumer_key, _from, auth) do
     {:reply, auth.consumer_key, auth}
+  end
+  def handle_call(:consumer_secret, _from, auth) do
+    {:reply, auth.consumer_secret, auth}
+  end
+  def handle_call(:access_token, _from, auth) do
+    {:reply, auth.access_token, auth}
+  end
+  def handle_call(:access_token_secret, _from, auth) do
+    {:reply, auth.access_token_secret, auth}
   end
 end
--- test/twutter_test.exs
+++ test/twutter_test.exs
@@ -8,24 +8,4 @@ defmodule TwutterTest do
   test "knows the home timeline url" do
     assert Twutter.home_timeline_url == "https://api.twitter.com/1.1/statuses/home_timeline.json"
   end
-
-  test "can configure a consumer_key" do
-    auth = Twutter.Authentication.new(
-      consumer_key: "1",
-      consumer_secret: "2",
-      access_token: "3",
-      access_token_secret: "4"
-    )
-    {:ok, client} = Twutter.REST.Client.start_link(auth)
-    assert Twutter.REST.Client.consumer_key(client) == "1"
-  end
-
-  test "can configure a consumer_secret" do
-  end
-
-  test "can configure an access_token" do
-  end
-
-  test "can configure an access_token_secret" do
-  end
 end
